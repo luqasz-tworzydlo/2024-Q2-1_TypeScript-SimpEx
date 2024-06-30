d5b9fc2aef7496ba8fb7b80e47e84cd05250df55
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2024-06-30 05:18:51 +0100

    Add files via upload

---
+++ 1_VariousTypeScriptExercises/src/TypeScriptExercises/A1_index.ts
@@ -0,0 +1,37 @@
+// testowanie (czy prawidłowo
+// zainstalowano typescript)
+
+const age: number = 23
+console.log(age)
+
+// instalacja typescript
+// npm i typescript -g
+// npm i ts-node
+
+// aby uruchomić program trzeba otworzyć terminal
+// oraz wpisać komendę: npx ts-node index.ts
+
+// typescript => 2 zadania do zrobienia [dodatkowe]
+// podwyższenie oceny końcowej o jeden stopień
+// poprzez zrobienie dwóch zadań dodatkowych
+
+// [Zadanie nr 1] Klasa Wykonawca:
+// - prywatne pola: nazwisko (typu String), piosenki (lista)
+// - publiczny konstruktor - ustawia nazwisko wykonawcy
+// - metody dodajPiosenke, usunPiosenke
+// - metoda toString
+
+// [Zadanie nr 2]
+// => Klasa osoba z polami nazwisko, wiek, konstruktorem i metodami toString
+// => Klasa SpisOsob
+// - prywatne pole osoby;
+// - metody dodajOsobe, usunOsobe;
+// - metoda wypisujaca aktualna listę;
+// - metoda znajdź zwracającą osobę z listy o podanym
+// nazwisku lub null, jeśli nie ma takiej osoby
+// - metody min, max, średnia zwracające odpowiednio
+// najmniejszy, największy i średni wiek osób z listy
+
+// https://pastebin.com/iCWQTtc1
+
+/// https://pastebin.com/nCjyhdkW
